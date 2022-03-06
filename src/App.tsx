14c04c71bbf98850cee4036f3293a1a17a900c5d
shcherbakov <kirriess@mail.ru>
2022-03-07 01:30:00 +0300

    wip: Начальная инициализация приложения

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="d-flex justify-content-center bg-secondary">
+        <h3>template</h3>
+      </div>
+      <div className="d-flex justify-content-around mt-2 border shadow align-items-center" style={{ height: '400px' }}>
+        <div className="d-flex align-self-start">хочу пиццы</div>
+        <div className="d-flex align-self-center">block2</div>
+        <div className="d-flex align-self-end">block3</div>
+      </div>
+      <div className="d-flex justify-content-around mt-2 border shadow align-items-center" style={{ height: '400px' }}>
+        <div className="d-flex align-self-start">хочу пиццы</div>
+        <div className="d-flex align-self-center">block2</div>
+        <div className="d-flex align-self-end">block3</div>
+      </div>
+      <div className="d-flex justify-content-around mt-2 border shadow align-items-center" style={{ height: '400px' }}>
+        <div className="d-flex align-self-start">хочу пиццы</div>
+        <div className="d-flex align-self-center">block2</div>
+        <div className="d-flex align-self-end">block3</div>
+      </div>
+      <div className="d-flex justify-content-around mt-2 border shadow align-items-center" style={{ height: '400px' }}>
+        <div className="d-flex align-self-start">хочу пиццы</div>
+        <div className="d-flex align-self-center">block2</div>
+        <div className="d-flex align-self-end">block3</div>
+      </div>
+      <div className="d-flex justify-content-around mt-2 border shadow align-items-center" style={{ height: '400px' }}>
+        <div className="d-flex align-self-start">хочу пиццы</div>
+        <div className="d-flex align-self-center">block2</div>
+        <div className="d-flex align-self-end">block3</div>
+      </div>
     </div>
   );
 }
